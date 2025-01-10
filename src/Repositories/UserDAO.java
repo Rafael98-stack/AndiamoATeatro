e2690f61a154664da38daa4aef9a1c60495879ff
Rafael98-stack <rafaelcasapaojr@gmail.com>
2025-01-10 20:57:14 +0100

    Developed getUserById in UserDAO.

--- src/Repositories/UserDAO.java
+++ src/Repositories/UserDAO.java
@@ -1,11 +1,34 @@
 package Repositories;
 
 import Configurations.JDBC;
+import Entities.User;
+import ExceptionHandlers.UserExceptions.UserNotFoundException;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class UserDAO {
     Connection connection = JDBC.getConnection();
 
-
+public User getUserById(Integer id) throws SQLException, UserNotFoundException {
+    User user = null;
+    String query = "SELECT * FROM User WHERE id =?";
+    PreparedStatement preparedStatement = connection.prepareStatement(query);
+    preparedStatement.setInt(1,id);
+    ResultSet resultSet = preparedStatement.executeQuery();
+    if (resultSet.next()){
+        user = new User(
+                resultSet.getInt("id"),
+                resultSet.getString("nome"),
+                resultSet.getString("cognome"),
+                resultSet.getString("email"),
+                resultSet.getString("indirizzo"),
+                resultSet.getString("telefono")
+        );
+        return user;
+    }
+   throw new UserNotFoundException("User non trovato.");
+}
 }
